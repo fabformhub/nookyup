706efc2713149847a6dc39c0149253581b2fd36e
Geoffrey Callaghan <irishgeoff@yahoo.com>
2026-03-11 18:55:30 +0000

    fixed dash and single view to show description

--- src/routes/ads.js
+++ src/routes/ads.js
@@ -1,12 +1,21 @@
 // routes/ads.js
 import express from "express";
-import { showCreateAdForm, showEditAdForm } from "../controllers/adsController.js";
+import { 
+  showCreateAdForm, 
+  showEditAdForm,
+  viewAd
+} from "../controllers/adsController.js";
 
 const router = express.Router();
 
-// Show create ad form
+// Create ad
 router.get("/ads/new", showCreateAdForm);
+
+// View ad 
+router.get("/ads/:id", viewAd);
+
+// Edit ad
 router.get("/ads/:id/edit", showEditAdForm);
 
-
 export default router;
+
